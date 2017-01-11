9bad24d74b9c9d7bd41e90c135d25272555aa5bc
Harry <harry@hogg.io>
2017-01-11 15:50:18 +0000

    Update 1-A*-search-each-move.js

--- attempts/1-A*-search-each-move.js
+++ attempts/1-A*-search-each-move.js
@@ -1,62 +1,68 @@
 /**
- * A* search with Manhattan heuristic (http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html)
- * It seems to calculate the shortest path ok but:
- * 1) Doesn't account for a future crash on that point
- * 2) Doesn't account for the environment after it has collected that point.
+ * @name findPath
+ * @description Function that is called at the beginning of each point, expecting
+ * an array of directions to be given to reach the X,Y coordinates of the square
+ * with the point on. The game will pause if the snake crashes or if the point
+ * is never reached.
  *
- * MAX POINTS: 70 something (But it's mainly luck)
+ * @param {Number} xMax Number of cells across the x axis.
+ * @param {Number} yMax Number of cells across the y axis.
+ * @param {Array} snake Array of [X,Y] cords of the snake from head to tail.
+ * @param {Array} point [X, Y] coordinates of the point.
+ * @param {Object} D Constant directions to be returned (UP, RIGHT, DOWN, LEFT)
+ *
+ * @return {Array} Moves for each cell i.e [D.UP, D.UP, D.RIGHT]
  */
- function findPath(xMax, yMax, snake, point, D) {
+function findPath(xMax, yMax, snake, point, DIR) {
+  const U = DIR.UP, D = DIR.DOWN, L = DIR.LEFT, R = DIR.RIGHT;
+  const moves = [];
+  const [pX, pY] = point;
+  
+  function shift(x, y, d) {
+    switch (d) {
+      case U: return [x, y - 1];
+      case R: return [x + 1, y];
+      case D: return [x, y + 1];
+      case L: return [x - 1, y];
+    }
+  }
+  
   function heuristic([x, y]) {
     return Math.abs(x - pX) + Math.abs(y - pY);
   }
-
-  function isInBounds([ x, y ]) {
-    return x >= 0 && x < xMax && y >= 0 && y < yMax;
+  
+  function isValid([, [x, y]]) {
+    return x >= 0 && x < xMax && y >= 0 && y < yMax &&
+      !snake.some(([sX, sY]) => x === sX && y === sY);
   }
-
-  function hasNotColided([ x, y ], snake) {
-    return !snake.find(([ sX, sY ]) => x === sX && y === sY);
-  }
-
-  function filterInvalid([d, p]) {
-    return isInBounds(p) && hasNotColided(p, snake);
-  }
-
-  function sortByHeuristic([aD, aP, aH], [bD, bP, bH]) {
+  
+  function byHeuristic([,, aH], [,, bH]) {
     return aH - bH;
   }
-
-  function shift([x, y], direction) {
-    switch (direction) {
-      case D.UP:    return [x, y - 1];
-      case D.RIGHT: return [x + 1, y];
-      case D.DOWN:  return [x, y + 1];
-      case D.LEFT:  return [x - 1, y];
+  
+  let [[x, y]] = snake;
+  
+  while (x !== pX || y !== pY) {
+    const SU = shift(x, y, U);
+    const SR = shift(x, y, R);
+    const SD = shift(x, y, D);
+    const SL = shift(x, y, L);
+    const winner = [
+      [U, SU, heuristic(SU)],
+      [R, SR, heuristic(SR)],
+      [D, SD, heuristic(SD)],
+      [L, SL, heuristic(SL)],
+    ].filter(isValid).sort(byHeuristic).shift();
+    
+    if (!winner) {
+      return moves; 
     }
+    
+    const [d, p] = winner;
+    
+    moves.push(d);
+    [[x, y]] = [p].concat(snake).slice(0, -1);
   }
-
-  const [pX, pY] = point;
-  const moves = [];
-  let nX, nY;
-
-  while (nX !== pX || nY !== pY) {
-    const directions = [
-      [D.UP, shift(snake[0], D.UP), heuristic(shift(snake[0], D.UP))],
-      [D.RIGHT, shift(snake[0], D.RIGHT), heuristic(shift(snake[0], D.RIGHT))],
-      [D.DOWN, shift(snake[0], D.DOWN), heuristic(shift(snake[0], D.DOWN))],
-      [D.LEFT, shift(snake[0], D.LEFT), heuristic(shift(snake[0], D.LEFT))],
-    ].filter(filterInvalid).sort(sortByHeuristic);
-
-    if (directions.length === 0) {
-      // Shit no moves... return so we can see where it ends up.
-      return moves;
-    }
-
-    moves.push(directions[0][0]);
-    snake = [shift(snake[0], directions[0][0])].concat(snake).slice(0, -1);
-    [nX, nY] = directions[0][1];
-  }
-
+  
   return moves;
 }
