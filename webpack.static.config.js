052ff08aa9221993acd0b91dc35c11d14754c9ec
Harry Hogg <harry@hogg.io>
2017-02-11 00:31:04 +0000

    gh-pages deploy

---
+++ webpack.static.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: 'assets/snake-heuristics.[hash].min.js',
+    path: 'static',
+    publicPath: '/snake-heuristics',
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          'postcss-loader',
+        ],
+      }),
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    }],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['static']),
+    new ExtractTextPlugin({
+      allChunks: true,
+      filename: 'assets/snake-heuristics.[hash].min.css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+  ],
+};
