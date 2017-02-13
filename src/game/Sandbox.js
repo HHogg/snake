b7d45c2c1df737e14e42673c2fa4d546ed97d062
Harry Hogg <harry@hogg.io>
2017-02-13 21:55:06 +0000

    Terminate worker on timeout and create a new one

--- src/game/Sandbox.js
+++ src/game/Sandbox.js
@@ -3,9 +3,13 @@ const sandboxWorker = require('raw-loader!./sandbox-worker.js');
 
 class Sandbox {
   constructor(onMessage, onError) {
-    this.worker = new Worker(window.URL.createObjectURL(new Blob([sandboxWorker])));
-    this.onError = onError;
     this.onMessage = onMessage;
+    this.onError = onError;
+    this.createWorker();
+  }
+
+  createWorker() {
+    this.worker = new Worker(window.URL.createObjectURL(new Blob([sandboxWorker])));
 
     this.worker.onerror = (error) => {
       this.reset();
@@ -21,8 +25,10 @@ class Sandbox {
   run(args) {
     this.worker.postMessage(args);
     this.timeout = setTimeout(() => {
+      this.worker.terminate();
+      this.createWorker();
       this.onError({ message: '⏰ Your code exceeded the maximum ' +
-        FN_TIMEOUT_SECONDS + 'seconds run time.' });
+        FN_TIMEOUT_SECONDS + ' seconds run time.' });
     }, FN_TIMEOUT_SECONDS * 1000);
   }
 
